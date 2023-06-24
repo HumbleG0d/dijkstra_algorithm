90772a661d93331c3cacd8ea5c8949aa7c9d83ed
HumbleG0d <sergiouni2000@gmail.com>
2023-06-24 07:07:27 -0500

    remove graph and add the code to get the graph from a text area , to convert this string to object use eval

--- js/index.js
+++ js/index.js
@@ -1,20 +1,23 @@
 import { dijkstra } from './dijkstra.js'
 import { drawGraph } from './graph.js'
 
-const graph = {
-	// A: { B: 4, C: 2 },
-	// B: { A: 4, C: 1, D: 5 },
-	// C: { A: 2, B: 1, D: 8 },
-	// D: { B: 5, C: 8 }
-	a: { b: 16, C: 10 , d: 5 },
-	b: { a: 16, c: 2, f: 4, g: 6},
-	c: { a: 10, b: 2 , d: 4,f: 12, e: 10},
-	d: { a: 5, c: 4 , e: 15},
-	e: { d: 15, c: 10, f: 3, z: 5},
-	f: { b: 4, c: 12, e: 3, g: 8 , z : 16},
-	g: { b: 6, f: 8, z: 7},
-	z: { e: 5, f: 16, g: 7}
-}
+/* const graph = {
+ 	a: { b: 16, c: 10 , d: 5 },
+ 	b: { a: 16, c: 2, f: 4, g: 6},
+ 	c: { a: 10, b: 2 , d: 4,f: 12, e: 10},
+ 	d: { a: 5, c: 4 , e: 15},
+ 	e: { d: 15, c: 10, f: 3, z: 5},
+ 	f: { b: 4, c: 12, e: 3, g: 8 , z : 16},
+ 	g: { b: 6, f: 8, z: 7},
+ 	z: { e: 5, f: 16, g: 7}
+ }
+*/
+const graph = document.getElementById('grafo')
+let grafo
+graph.addEventListener('keyup' , () => {
+	grafo = graph.value
+})
+
 
 const nodo_inicial = document.getElementById('nodo_inicial')
 let nodo1
@@ -32,21 +35,27 @@ nodo_final.addEventListener('keyup' , () => {
 let bandera = false
 let highlightedEdges = []
 
+const mostrar = document.getElementById('view')
+mostrar.addEventListener('click' , (e) => {
+	e.preventDefault()
+	drawGraph(eval('(' + grafo + ')') , highlightedEdges , bandera)
+})
+
 const calcular = document.getElementById('button')
 calcular.addEventListener('click' , (e) => {
 	e.preventDefault()
 	const p = document.getElementById('path')
 	const p2 = document.getElementById('distance')
     
-	const {path,distance} = dijkstra(graph , nodo1 , nodo2)
+	const {path,distance} = dijkstra(eval('(' + grafo + ')'), nodo1 , nodo2)
 	bandera = true
 	highlightedEdges = []
 	for(let i = 0 ; i < path.length-1 ; i++){
 		highlightedEdges.push({from: path[i] , to: path[i+1]})
 	}
-	drawGraph(graph , highlightedEdges , bandera)
+	drawGraph(eval('(' + grafo + ')')  , highlightedEdges , bandera)
 	p.innerHTML = `Camino : ${path.join('->')}`
 	p2.innerHTML = `Distancia: ${distance}`
 })
 
-if(!bandera)drawGraph(graph , highlightedEdges , bandera)
+// if(!bandera)drawGraph(graph , highlightedEdges , bandera)
